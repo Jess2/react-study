d2cb63d42ffbf26fbd9df683e3cd7b9474040d84
Soyeon Jung <jess2.jsy@gmail.com>
2018-11-12 17:45:17 +0900

    react 기본 사용법 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // 리액트와 그 내부의 컴포넌트를 불러온다.
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 // 클래스를 통해 컴포넌트를 만들 수 있다.
@@ -8,23 +7,68 @@ import './App.css';
 // JSX를 return 해주어야 한다.
 class App extends Component {
   render() {
+    // 변수
+    const name = 'Jess2';
+    let age = 27;
+    age = 28;
+
+    // 스타일 변수
+    const myStyle = {
+      background: 'yellow',
+      color: 'red'
+    };
+
+    const yourStyle = {
+      background: 'blue',
+      fontSize: '30px',
+      color: 'white'
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        {/* 태그는 꼭 닫혀야한다 */}
+        <input type="text" />
+
+        {/* 두 개 이상의 엘리먼트는 하나의 엘리먼트로 감싸져 있어야 한다. */}
+        <div>Hello</div>
+        <div>Bye</div>
+
+        {/* JSX 내부에서 자바스크립트 값을 사용할 떄는 {}를 사용한다 */}
+        <div>hello {name}! My age is {age}</div>
+
+        {/* 조건부 렌더링 - 삼항연산자 */}
+        {
+          1 + 1 === 2
+            ? (<div>맞아요!</div>)
+            : (<div>틀려요!</div>)
+        }
+
+        {/* 조건부 렌더링 - && */}
+        {
+          1 + 1 === 2 && (<div>맞아요!</div>)
+        }
+
+        {/* 조건부 렌더링 - IIFE, if */}
+        {
+          (() => {
+            if (age === 27) return (<div>2018년</div>);
+            if (age === 28) return (<div>2019년</div>);
+            else return (<div>다른 경우</div>);
+          })()
+        }
+
+        <div style={myStyle}>
+          스타일이 적용되었죠?
+        </div>
+
+        <div style={yourStyle}>
+          다른 스타일이 적용되었어요
+        </div>
+
+        <div className="myClass">
+          클래스로 스타일을 적용해봐요
+        </div>
+      </Fragment>
     );
   }
 }
