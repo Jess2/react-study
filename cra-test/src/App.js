038c795e3d72ee476288707898edac6ee9e2bc76
jess2 <jess2.jsy@gmail.com>
2020-08-18 23:34:20 +0900

    create new project

---
+++ cra-test/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function onClick() {
+    import('./data.json').then(({default: data}) => {
+      console.log({ data });
+    })
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={onClick}>데이터 보여주기</button>
+        <h1>Hello, Jess2!</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ pure-react-test/simple1-jsx.js
@@ -0,0 +1,25 @@
+// jsx 로 구현
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text);
+}
+
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("span", null, "\uD604\uC7AC \uCE74\uC6B4\uD2B8: "), /*#__PURE__*/React.createElement("span", {
+    style: {
+      marginRight: 10,
+      color: 'red'
+    }
+  }, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
+}
+
+const DOM_CONTAINER = document.getElementById('root');
+ReactDOM.render(React.createElement(Container), DOM_CONTAINER);
