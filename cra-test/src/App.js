eac28027bebf4240b69b498713564424f9374202
jess2 <jess2.jsy@gmail.com>
2020-08-22 18:54:00 +0900

    [Feat] react-router-dom을 이용한 SPA 라우팅 처리

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Rooms from './Rooms';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   function onClick() {
     import('./data.json').then(({default: data}) => {
       console.log({ data });
@@ -14,20 +16,28 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={onClick}>데이터 보여주기</button>
         <h1>Hello, Jess2!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <div>
+            <Link to="/">홈</Link>
+            <br />
+            <Link to="/photo">사진</Link>
+            <br />
+            <Link to="/rooms">방 소개</Link>
+            <br />
+            <Route exact path="/" component={Home} />
+            <Route path="/photo" component={Photo} />
+            <Route path="/rooms" component={Rooms} />
+          </div>
+        </BrowserRouter>
       </header>
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>이곳은 홈페이지입니다.</h2>;
+}
+
+function Photo() {
+  return <h2>여기서 사진을 감상하세요.</h2>;
+}
---
+++ cra-test/src/Rooms.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export default function Rooms({ match }) {
+  return (
+    <div>
+      <h2>여기는 방을 소개하는 페이지입니다.</h2>
+      <Link to={`${match.url}/blueRoom`}>파란 방</Link>
+      <br />
+      <Link to={`${match.url}/greenRoom`}>초록 방</Link>
+      <br />
+      <Route path={`${match.url}/:roomId`} component={Room} />
+      <Route exact path={match.url} render={() => <h3>방을 선택하세요</h3>} />
+    </div>
+  )
+}
+
+function Room({ match }) {
+  return <h2>{`${match.params.roomId}방이 선택 됨`}</h2>;
+}
