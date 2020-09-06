651810a9aafacd0af45210e4ebc1c07000cc9b87
jess2 <jess2.jsy@gmail.com>
2020-09-06 17:43:24 +0900

    [Feat] 로컬 스토리지를 사용하여 Best Score 계산 기능 추가

--- game-2048/src/App.js
+++ game-2048/src/App.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './component/Header';
 import AboveGame from './component/AboveGame';
 import Game from './component/Game';
+import useLocalStorageNumber from "./hook/useLocalStorage";
 
 export default function App() {
   const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorageNumber('bestScore', 0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  });
+
   return (
     <div className="container">
-      <Header score={score} bestScore={0}/>
+      <Header score={score} bestScore={bestScore}/>
       <AboveGame />
       <Game setScore={setScore}/>
     </div>
---
+++ game-2048/src/hook/useLocalStorage.js
@@ -0,0 +1,24 @@
+import {useEffect, useState} from "react";
+
+export default function useLocalStorageNumber(key, initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    const valueStr = window.localStorage.getItem(key);
+
+    if (valueStr) {
+      setValue(Number(valueStr));
+    }
+  }, [key]);
+
+  useEffect(() => {
+    const prev = window.localStorage.getItem(key);
+    const next = String(value);
+
+    if (prev !== next) {
+      window.localStorage.setItem(key, next);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+}
